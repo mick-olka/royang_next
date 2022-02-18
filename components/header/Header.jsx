5a3d7ba2c0c0531d1c7390a3df3a1e870639dd87
mick-olka <mickolka64@gmail.com>
2022-02-18 20:22:08 +0200

    phones popup for mobiles

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -9,26 +9,37 @@ import LocalesPane from "../locales/LocalesPane";
 const Header = ({links, headerText, navShow, setNavShow}) => {
 
     const [headerBorder, setHeaderBorder] = useState(false);
+    const [showPhones, setShowPhones] = useState(false);    //  for mobiles
+    const handleScroll = () => {
+        (window.scrollY > 20) ? setHeaderBorder(true) : setHeaderBorder(false);
+        setShowPhones(false);
+    }
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
         return () => {
             window.removeEventListener("scroll", handleScroll);
         }
     }, [headerBorder]);
-    const handleScroll = () => {
-        (window.scrollY > 20) ? setHeaderBorder(true) : setHeaderBorder(false);
-    }
-
     let header_style = {
         transition: "all 300ms ease-in",
         boxShadow: headerBorder ? "2px 2px 3px 0 rgba(0,0,0,0.55)" : "none",
     }
+
+    const phones_ul =()=> {return <ul>
+        <li className={s.phone_li}><a href={"tel:" + global_data.phones[0]}>{global_data.phones[0]}</a>
+        </li>
+        <li className={s.phone_li}><a href={"tel:" + global_data.phones[1]}>{global_data.phones[1]}</a>
+        </li>
+        <li className={s.phone_li}><a href={"tel:" + global_data.phones[2]}>{global_data.phones[2]}</a>
+        </li>
+    </ul>};
 
     return (
         <div className={s.header_container} style={header_style}>
             <div className={s.header}>
                 <div className={s.menu_btn} onClick={()=>setNavShow(!navShow)} >{navShow ? "×" : "≡" }</div>
                 <div className={s.name_div}>
+                    <div className={s.header_name} >
                     <Link href={'/'} passHref>
                         <a>
                             <h1>
@@ -43,21 +54,18 @@ const Header = ({links, headerText, navShow, setNavShow}) => {
                             </h1>
                         </a>
                     </Link>
+                    </div>
+                    <div className={s.contacts_block_mobile} >
+                        <p onClick={()=>setShowPhones(!showPhones)} id='phones_popup_btn' >+380963963930 {showPhones?'△':'▽'}</p>
+                        <div className={s.phones_popup} style={showPhones?{display: "block"}:{display: "none"}} >
+                        {phones_ul()}
+                    </div></div>
                 </div>
-                <div className={s.header_text_div}>
+                <div className={s.header_text_div} style={headerBorder?{display: "none"}:{display: "inherit"}} >
                     <p style={{fontSize: "1.2rem"}}>{headerText}</p>
                 </div>
                 <div className={s.phones_div}>
-                    <ul>
-
-                        <li className={s.phone_li}><a href={"tel:" + global_data.phones[0]}>{global_data.phones[0]}</a>
-                        </li>
-                        <li className={s.phone_li}><a href={"tel:" + global_data.phones[1]}>{global_data.phones[1]}</a>
-                        </li>
-                        <li className={s.phone_li}><a href={"tel:" + global_data.phones[2]}>{global_data.phones[2]}</a>
-                        </li>
-
-                    </ul>
+                    {phones_ul()}
                 </div>
                 <div className={s.call_time_div}>
                     <p>9:00 - 20:00</p>
