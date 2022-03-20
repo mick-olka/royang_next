c743b307e9632e5da3de474b652b3b482c87ee6f
mick-olka <mickolka64@gmail.com>
2022-03-20 22:59:18 +0200

    back_url_global

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+const global_data = require("./utils/global_data");
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['192.168.1.164', '178.54.240.228', 'static-sl.insales.ru'],
+    domains: [ global_data.backURL, '192.168.1.164', '178.54.240.228', 'static-sl.insales.ru'],
   },
   i18n: {
     locales: ["ua", "ru"],
--- utils/global_data.js
+++ utils/global_data.js
@@ -7,6 +7,7 @@ let global_data = {
         "(096) 396 30 32",
     ],
     text_boxes: [],
+    backURL: '185.65.245.26',
 }
 
 export default global_data;
