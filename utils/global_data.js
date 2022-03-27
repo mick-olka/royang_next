2f08a62763383bf3b2f94d2d902aba0fd6535362
mick-olka <mickolka64@gmail.com>
2022-03-27 13:10:55 +0300

    phones_update

--- utils/global_data.js
+++ utils/global_data.js
@@ -2,12 +2,12 @@
 let global_data = {
     page_limit: 10,
     phones: [
-        "(096) 396 39 30",
-        "(096) 396 30 31",
-        "(096) 396 30 32",
+        "(044) 383 58 53",
+        "(096) 565 90 71",
+        "(095) 595 98 03",
     ],
     text_boxes: [],
-    backURL: '185.65.245.26',
-    adminURL: 'http://185.65.245.26:3005/'
+    backURL: '192.168.0.113',
+    adminURL: 'http://192.168.0.103:3005/'
 }
 module.exports = global_data;
