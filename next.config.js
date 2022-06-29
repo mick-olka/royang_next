d29f12011b2dc4fff6b5bfbb08902570b07ee969
mick-olka <mickolka64@gmail.com>
2022-06-29 12:17:04 +0300

    update: api url

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const global_data = require("./utils/global_data");
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: [ global_data.backURL, '192.168.1.164', '178.54.240.228', 'static-sl.insales.ru'],
+    domains: [ global_data.backURL, '192.168.1.164', '178.54.240.228', '185.65.245.26'],
   },
   i18n: {
     locales: ["ua", "ru"],
--- utils/global_data.js
+++ utils/global_data.js
@@ -10,7 +10,7 @@ let global_data = {
     text_boxes: [],
     // backURL: '192.168.0.113',
     // adminURL: 'http://192.168.0.113:3005/'
-    backURL: '185.65.245.26',
+    backURL: 'rotang.biz',
     adminURL: 'http://185.65.245.26:3005/'
 }
 module.exports = global_data;
